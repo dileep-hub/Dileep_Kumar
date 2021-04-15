fcd7e06273f9856645c8b624a7eb318e9478412a
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2021-04-15 22:33:36 +0530

    Update sum_of_n.c

--- sum_of_n.c
+++ sum_of_n.c
@@ -1,47 +1,45 @@
-#include<stdio.h>
-
-
-int readarr(int n,int* arr)
+#include <stdio.h>
+int input_n()
 {
-    for(int i=0;i<n;i++)
-    {
-        printf("\n Enter number:");
-        scanf("%d",&arr[i]);
-    }
-    
-    return 0;
+    int n;
+    printf("Enter a number of elements: \n");
+    scanf("%d",&n);
+    return n;
 }
-
-int calc_sum(int n,int* arr)
+void input_array(int n, float a[n])
 {
-    int sum=0 ;
-    for(int i=0;i<n;i++)
-  {
-      printf("%d \n",arr[i]);
-  }
     for(int i=0;i<n;i++)
     {
-        sum +=arr[i];
+        printf("Enter the element no %d of the array\n",i+1);
+        scanf("%f",&a[i]);
+    }
+}
+float array_sum(int n, float a[n])
+{
+    float sum=0;
+    for(int i=0;i<n;i++) 
+    {
+        sum += a[i];
     }
     return sum;
 }
-
-int display(int* sum)
+void output(int n, float a[n], float sum)
 {
-    printf("\n Sum is: %d \n",*sum);
-    return 0;
+    int i;
+    printf("The sum of "); 
+    for(i=0;i<n-1;i++) 
+    {
+        printf("%f+",a[i]);
+    }
+    printf("%f=%.2f",a[i],sum);
 }
-
-
 int main()
 {
-  int n,sum=0;
-  printf("Enter the n value: ");
-  scanf("%d",&n);
-  
-  int arr[n];
-  readarr(n,arr);
-  sum = calc_sum(n,arr);
-   display(&sum);
-  return 0;
+    int n;
+    float sum,a[n];
+    n = input_n();
+    input_array(n,a);
+    sum=array_sum(n,a);
+    output(n,a,sum);
 }
+
